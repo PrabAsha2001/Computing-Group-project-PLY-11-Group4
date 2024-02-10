0663e51eb7c6bdab52c70a774500900c409b79ce
PrabAsha2001 <umiasha79@gmail.com>
2024-02-10 14:04:02 +0530

    Prabasha's commit.Did some modifications  to  UserLogin.fxml, UserLoginController and created dashboard.fxml file.

--- src/main/java/com/invento/invento/UserLoginController.java
+++ src/main/java/com/invento/invento/UserLoginController.java
@@ -1,10 +1,15 @@
 package com.invento.invento;
 
 import com.invento.invento.javaClass.DatabaseConnection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,12 +26,27 @@ public class UserLoginController {
     @FXML
     private TextField txtPhoneNo;
 
+    @FXML
+    private Label lblMessage;
+
+    public void AdminloginButtonOnAction(ActionEvent event){
+
+        if(txtPhoneNo.getText().isEmpty() || txtPassword.getText().isEmpty()){
+            lblMessage.setText("Please enter Phone Number and Password!");
+
+        }else{
+            LoginAdmin();
+        }
+
+    }
     public void LoginAdmin(){
         //Created an object from databaceConnection class and created database connection
         DatabaseConnection connectNow=new DatabaseConnection();
         Connection connectDB=connectNow.getConnection();
 
-        String sql="SELECT * FROM admin WHERE contact=? AND password=?";
+        String type;
+
+        String sql="SELECT * FROM employee WHERE contact=? AND password=?";
         try{
             //Executing the sql statement
             PreparedStatement preparedStatement=connectDB.prepareStatement(sql);
@@ -34,27 +54,34 @@ public class UserLoginController {
             preparedStatement.setString(1,txtPhoneNo.getText());
             preparedStatement.setString(2,txtPassword.getText());
 
-            ResultSet rs=preparedStatement.executeQuery();
-            Alert alert;
+            try(ResultSet rs=preparedStatement.executeQuery()){
 
 
-            if(txtPhoneNo.getText().isBlank() || txtPassword.getText().isBlank()){
-                //display an alert if username or password is empty
-                alert=new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error Message");
-                alert.setHeaderText(null);
-                alert.setContentText("Please enter the Phone Number and Password");
-            }else{
                 if(rs.next()){
+                    type=rs.getString("type");
+
+                    if(type!="admin"){
+                        btnLogin.getScene().getWindow().hide();
+                        Parent root= FXMLLoader.load(getClass().getResource("AdminDashboard.fxml"));
+                        Stage stage=new Stage();
+                        Scene scene=new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
+
+                    }
+
 
                 }else{
-                    //display an error message is username or password is wrong
-                    alert=new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error Message");
-                    alert.setHeaderText(null);
-                    alert.setContentText("Wrong Phone Number or Password");
+                    lblMessage.setText("Phone Number or Password is wrong");
                 }
+
+
             }
+
+
+
+
+
         }catch(Exception e){
             e.printStackTrace();
         }
